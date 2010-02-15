38f1c5d4765e1485653c2796ecc990c3af058254
Philip Schatz <phil@cnx.org>
2010-02-15 21:52:23 +0000

    Changed /mycnx to /mydashboard . Prefixing with "my" so it more closely resembles the MyCNX/MyRhaptos label. Fixes #10166

--- Products/Lensmaker/skins/lensmaker/inLensEdit.py
+++ Products/Lensmaker/skins/lensmaker/inLensEdit.py
@@ -1,5 +1,5 @@
 ## tell, based on URL, if we're "editing" a lens or lens organizer (or, really, anything under 'lenses')
-## used to switch back and forth between mycnx and lens tabs depending on editing or not
+## used to switch back and forth between mydashboard and lens tabs depending on editing or not
 ## called from actions, so we can check object and URL
 
 request = context.REQUEST
@@ -7,7 +7,7 @@ url = request.URL
 view = url.split('/')[-1]
 
 # I chose a whitelist, because forums add a whole lot of unknown to a blacklist,
-# and it seems better to fail to 'lenses' than to fail to 'mycnx'
+# and it seems better to fail to 'lenses' than to fail to 'mydashboard'
 editmode = view in ('edit', 'contents',                                    # aliases
                     'lens_content_view', 'lens_edit', 'lens_preview',      # std lens tab templates
                     'lens_delete_confirm', 'lens_content_edit',            # other std lens edit templates
